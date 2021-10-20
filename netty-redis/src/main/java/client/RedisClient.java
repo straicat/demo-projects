106b3ecf1f9c662f2da5fca1b4b6eef1634b29c6
jlice <1121672253@qq.com>
2021-10-20 12:56:02 +0800

    update

--- netty-redis/src/main/java/client/RedisClient.java
+++ netty-redis/src/main/java/client/RedisClient.java
@@ -2,17 +2,24 @@ package client;
 
 import codec.RedisResponseDecoder;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
 
 public class RedisClient {
-    public static void main(String[] args) {
-        Bootstrap bootstrap = new Bootstrap();
-        EventLoopGroup group = new NioEventLoopGroup();
+    private Bootstrap bootstrap;
+    private EventLoopGroup group;
+    private Channel channel;
+
+    public RedisClient(InetSocketAddress address) {
+        bootstrap = new Bootstrap();
+        group = new NioEventLoopGroup();
+
         try {
             bootstrap.group(group)
                     .channel(NioSocketChannel.class)
@@ -24,13 +31,29 @@ public class RedisClient {
                             ch.pipeline().addLast(new RedisClientHandler());
                         }
                     });
-
-            ChannelFuture future = bootstrap.connect(new InetSocketAddress("127.0.0.1", 6379)).sync();
-            future.channel().closeFuture().sync();
+            ChannelFuture future = bootstrap.connect(address).sync();
+            channel = future.channel();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             group.shutdownGracefully();
         }
+    }
+
+    public void get(String key) {
+        channel.writeAndFlush(Unpooled.copiedBuffer("GET " + key + "\n", StandardCharsets.UTF_8));
+    }
+
+    public void close() {
+        if (channel != null) {
+            channel.close();
+        }
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        RedisClient client = new RedisClient(new InetSocketAddress("127.0.0.1", 6379));
+        client.get("test");
+        client.close();
     }
 }
--- netty-redis/src/main/java/client/RedisClientHandler.java
+++ netty-redis/src/main/java/client/RedisClientHandler.java
@@ -1,17 +1,10 @@
 package client;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-import java.nio.charset.StandardCharsets;
-
 public class RedisClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(Unpooled.copiedBuffer("GET test\n", StandardCharsets.UTF_8));
-    }
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
