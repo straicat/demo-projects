4f5877bd5b9b0e5feebb48cd51eabec8ac4b5b70
straicat <1121672253@qq.com>
2023-05-29 01:10:30 +0800

    添加SQL

---
+++ tiny-url/src/main/resources/sql/ddl.sql
@@ -0,0 +1,11 @@
+create table tiny_url_mapping
+(
+    id        bigint auto_increment comment '主键ID'
+        primary key,
+    short_url varchar(8)    default '' not null comment '短URL',
+    long_url  varchar(1024) default '' not null comment '长URL',
+    ctime     bigint        default 0  not null comment '创建时间',
+    constraint idx_short_url
+        unique (short_url)
+)
+    comment 'URL映射表' collate = utf8mb4_unicode_ci;
